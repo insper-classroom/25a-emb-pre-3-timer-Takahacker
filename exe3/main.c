7ebed9f47756135b87511fb3df0129a7d921c1bc
Gustavo Takahashi <gustavoet@al.insper.edu.br>
2025-03-12 14:14:50 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -1,5 +1,6 @@
-#include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
@@ -10,26 +11,26 @@ volatile bool led_state = false;
 volatile bool flag_alarm = false;
 volatile bool timer_fired = false;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { 
-        if (gpio == BTN_PIN_R) {
-            flag_f_r = 1;
-            flag_alarm = 0;
-            add_alarm_in_ms(500, alarm_callback, NULL, false);
-        }
-    } else if (events == 0x8) { 
-        if (gpio == BTN_PIN_R) {
-            flag_f_r = 0;
-            flag_alarm = 1;
-        }
-    }
-}
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     if (!flag_alarm) { 
         timer_fired = true;
     }
     return 0;
+}
+
+void btn_callback(uint gpio, uint32_t events) {
+    if (gpio == BTN_PIN_R) {
+        if (events & GPIO_IRQ_EDGE_FALL) { 
+            flag_f_r = 1;
+            flag_alarm = false;
+            add_alarm_in_ms(500, alarm_callback, NULL, false);
+        }
+        if (events & GPIO_IRQ_EDGE_RISE) { 
+            flag_f_r = 0;
+            flag_alarm = true;
+        }
+    }
 }
 
 int main() {
@@ -44,7 +45,7 @@ int main() {
     gpio_pull_up(BTN_PIN_R);
 
     gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, btn_callback);
 
     while (true) {
         if (timer_fired) {
@@ -52,5 +53,6 @@ int main() {
             gpio_put(LED_PIN_R, led_state);
             timer_fired = false;
         }
+        sleep_ms(10);  
     }
 }
