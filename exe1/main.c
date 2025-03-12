51b21340f77478b6cf17e5a3109a33255d9e224b
Gustavo Takahashi <gustavoet@al.insper.edu.br>
2025-03-12 13:30:03 -0300

    ex1.1

--- exe1/main.c
+++ exe1/main.c
@@ -5,8 +5,9 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
-struct repeating_timer timer;
+
+volatile int flag_f_r = 0;          
+struct repeating_timer timer;         
 
 bool timer_callback(struct repeating_timer *t) {
     gpio_xor_mask(1u << LED_PIN_R);  
@@ -21,17 +22,21 @@ void btn_callback(uint gpio, uint32_t events) {
 
 int main() {
     stdio_init_all();
+    
+    
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
-    gpio_put(LED_PIN_R, 0); 
+    gpio_put(LED_PIN_R, 0);  
+    
     
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
     
+    
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-    bool is_timer_active = false;  // Local variable to track timer state
+    bool is_timer_active = false;  
     while (true) {
         if (flag_f_r) {
             flag_f_r = 0;
@@ -42,7 +47,7 @@ int main() {
                 }
             } else {
                 cancel_repeating_timer(&timer);
-                gpio_put(LED_PIN_R, 0);
+                gpio_put(LED_PIN_R, 0);  
                 is_timer_active = false;
             }
         }
